bff6e758b1a9330f448a2b710f0697e0a38a4663
Cris Lom <bigc4997@gmail.com>
2020-12-30 22:52:41 -0500

    starting to work to include in eug site, needs styles

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './app';
 
 
+
 ReactDOM.render(
     <React.Fragment>
         <App />
     </React.Fragment>,
-    document.getElementById('root'),
+    document.getElementById('plantDatabase'),
 );
--- src/seach-bar.component.js
+++ src/seach-bar.component.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 function SeachBar(props) {
-
-    // console.log(props);
     return(
         <div className="searchBar">
             <input type="text" id="search" value={props.searchValue} onChange={(e) => {props.searchInput(e)}}/>
